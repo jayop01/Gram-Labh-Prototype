99e5adde9e2b707dcebbdc8ce60b5887f0ad190c
Jay Jaiswal <jaiswal.jay2006@gmail.com>
2025-10-07 02:28:43 +0530

    Renaming prototype.html to index.html for better GitHub Pages compatibility.

---
+++ sources.js
@@ -0,0 +1,7 @@
+const GOVERNMENT_LINKS = {
+    'HIGH_GRANT': 'https://krishi.maharashtra.gov.in/en/schemes',
+    'MAHADB_LINKED': 'https://mahadbtmahait.gov.in/Home/Index',
+    'HORTICULTURE_SCHEME': 'https://horticulture.maharashtra.gov.in/',
+    'LOW_RISK': 'https://pmfby.gov.in/',
+    'NONE': '#'
+};
